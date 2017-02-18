0923b3b89c498660aaf1d2eb4f7ca589423015ab
mferten <mferten@yahoo.com>
2017-02-17 21:15:41 -0500

    commit all

--- phpfiles/WorldCountries.php
+++ phpfiles/WorldCountries.php
@@ -1,1 +1,69 @@
-?php
+<?php
+
+namespace DWA;
+
+class WorldCountries {
+
+    /**
+	* Properties
+	*/
+    private $flags;
+    private $continents;
+    private $populations;
+    private $capitals;
+
+
+    /**
+	* a Flag and the country (array)
+	*/
+    public function __construct() {
+
+        /* get the flags for the countries */
+        $dataJson = file_get_contents($jsonFlags);
+        $this->flags = json_decode($dataJson , true);
+
+        /* get the continents for the countries */
+        $dataJson = file_get_contents($jsonContinents);
+        $this->$continents= json_decode($dataJson , true);
+
+        /* get the populations for the countries */
+        $dataJson = file_get_contents($jsonPopulations);
+        $this->$populations= json_decode($dataJson , true);
+
+        /* get the capitals for the countries */
+        $dataJson = file_get_contents($jsonCapitals);
+        $this->$capitals= json_decode($dataJson , true);
+
+    }
+
+    /**
+	* Getter for $flags property
+	*/
+    public function getFlags() {
+        return $this->$flags;
+    }
+
+    /**
+	* Getter for $continents property
+	*/
+    public function getContinents() {
+        return $this->$continents;
+    }
+
+
+    /**
+	* Getter for $populations property
+	*/
+    public function getPopulations() {
+        return $this->$populations;
+    }
+
+
+    /**
+	* Getter for $capitals property
+	*/
+    public function getCapitals() {
+        return $this->$capitals;
+    }
+
+} # end of class
