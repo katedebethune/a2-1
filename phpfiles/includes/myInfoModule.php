da26a19ed80371a3135831754bce08c3cd3ffce1
mferten <mferten@yahoo.com>
2017-02-18 19:42:16 -0500

    commit all

--- phpfiles/includes/myInfoModule.php
+++ phpfiles/includes/myInfoModule.php
@@ -246,33 +246,36 @@
                             <div class="floatingInput inputTopMargin"><label>Population
                                 <select name='population' id='population'>
                                     <option value='choose'>Choose one...</option>
-                                    <option value='<1' <?php if (isset($_GET['population']) && $_GET['population'] == '1000000') echo 'SELECTED'?>>Less than 1 Million</option>
-                                    <option value='>=1<5' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>More than Equal to 1 Less than 5 Millions</option>
-                                    <option value='>=5<10' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>5 to 10 Millions</option>
-                                    <option value='20000000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>10 to 20 Millions</option>
-                                    <option value='30000000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>21 to 5 Millions</option>
-                                    <option value='4000000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>1 to 5 Millions</option>
-                                    <option value='5000000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>1 to 5 Millions</option>
-                                    <option value='7500000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>1 to 5 Millions</option>
-                                    <option value='10000000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>1 to 5 Millions</option>
-                                    <option value='15000000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>1 to 5 Millions</option>
-                                    <option value='20000000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>1 to 5 Millions</option>
-                                    <option value='25000000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>1 to 5 Millions</option>
-                                    <option value='30000000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>1 to 5 Millions</option>
-                                    <option value='35000000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>1 to 5 Millions</option>
-                                    <option value='50000000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>1 to 5 Millions</option>
-                                    <option value='10000000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>1 to 5 Millions</option>
-                                    <option value='5000000' <?php if (isset($_GET['population']) && $_GET['population'] == '5000000') echo 'SELECTED'?>>1 to 5 Millions</option>
+                                    <option value='1' <?php if (isset($_GET['population']) && $_GET['population'] == '1') echo 'SELECTED'?>>< 1 Million</option>
+                                    <option value='1,5' <?php if (isset($_GET['population']) && $_GET['population'] == '1,5') echo 'SELECTED'?>>>= 1 < 5 Millions</option>
+                                    <option value='5,10' <?php if (isset($_GET['population']) && $_GET['population'] == '5,10') echo 'SELECTED'?>>>= 5 < 10 Millions</option>
+                                    <option value='10,15' <?php if (isset($_GET['population']) && $_GET['population'] == '10,15') echo 'SELECTED'?>>>= 10 < 15 Millions</option>
+                                    <option value='15,20' <?php if (isset($_GET['population']) && $_GET['population'] == '15,20') echo 'SELECTED'?>>>= 15 < 20 Millions</option>
+                                    <option value='20,30' <?php if (isset($_GET['population']) && $_GET['population'] == '20,30') echo 'SELECTED'?>>>= 20 < 30 Millions</option>
+                                    <option value='30,40' <?php if (isset($_GET['population']) && $_GET['population'] == '30,40') echo 'SELECTED'?>>>= 30 < 40 Millions</option>
+                                    <option value='40,50' <?php if (isset($_GET['population']) && $_GET['population'] == '40,50') echo 'SELECTED'?>>>= 40 < 50 Millions</option>
+                                    <option value='50,60' <?php if (isset($_GET['population']) && $_GET['population'] == '50,60') echo 'SELECTED'?>>>= 50 < 60 Millions</option>
+                                    <option value='60,70' <?php if (isset($_GET['population']) && $_GET['population'] == '60,70') echo 'SELECTED'?>>>= 60 < 70 Millions</option>
+                                    <option value='80,100' <?php if (isset($_GET['population']) && $_GET['population'] == '80,100') echo 'SELECTED'?>>>= 70 < 100 Millions</option>
+                                    <option value='100,150' <?php if (isset($_GET['population']) && $_GET['population'] == '100,150') echo 'SELECTED'?>>>= 100 < 150 Millions</option>
+                                    <option value='150,200' <?php if (isset($_GET['population']) && $_GET['population'] == '150,200') echo 'SELECTED'?>>>= 150 < 200 Millions</option>
+                                    <option value='200,250' <?php if (isset($_GET['population']) && $_GET['population'] == '200,250') echo 'SELECTED'?>>>= 200 < 250 Millions</option>
+                                    <option value='250,300' <?php if (isset($_GET['population']) && $_GET['population'] == '250,300') echo 'SELECTED'?>>>= 250 < 300 Millions</option>
+                                    <option value='300-500' <?php if (isset($_GET['population']) && $_GET['population'] == '300-500') echo 'SELECTED'?>>>= 300 < 500 Millions</option>
+                                    <option value='500,1000' <?php if (isset($_GET['population']) && $_GET['population'] == '500,1000') echo 'SELECTED'?>>>= 500 Millions < 1 Billion</option>
+                                    <option value='1000,2000' <?php if (isset($_GET['population']) && $_GET['population'] == '1000,2000') echo 'SELECTED'?>>>= 1 Billions < 2 Billions</option>
                                 </select></label>
                             </div>
+                            <!-- options will be read via PHP through a JSON file -->
                             <div class="floatingInput">
                                 <label>Language
                                     <select name='language' id='language'>
                                         <option value='choose'>Choose one...</option>
-                                        <option value='<1' <?php if (isset($_GET['language']) && $_GET['language'] == 'English') echo 'SELECTED'?>>English</option>
+                                        <option value='English' <?php if (isset($_GET['language']) && $_GET['language'] == 'English') echo 'SELECTED'?>>English</option>
                                     </select>
                                 </label>
                             </div>
+                            <!-- only one letter as the first of a capital name can be entered -->
                             <div class="floatingInput">
                                 <label>Capital
                                     <input type='input' name='capital' id='capital' value='<?php if(isset($_GET['capital'])) echo $_GET['capital'] ?>'>
